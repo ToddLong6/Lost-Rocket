9277845f3a3e964de9a3f5112bf41927edb9e080
Todd Long <hartforddavid@yahoo.com>
2022-04-19 16:47:21 -0400

    Loading Level

--- Assets/Scripts/CollisonHandler.cs
+++ Assets/Scripts/CollisonHandler.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisonHandler : MonoBehaviour
 {
+    [SerializeField] float delayload = 1f;
     void OnCollisionEnter(Collision other) 
     {
         //Switch Statement are a Conditional like If/Else Statement
@@ -13,14 +15,44 @@ public class CollisonHandler : MonoBehaviour
                 Debug.Log("Im Not dead"); 
                 break;
             case "Finish":
-                Debug.Log("I WON!!"); 
+                NextLevel();
                 break;
             case "Fuel":
                 Debug.Log("I can still fly"); 
                 break;
             default:
-                Debug.Log("Im Dead");
+                StartCrashSequence();
                 break;
         }
+    }
+    void NextLevel()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", delayload);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delayload);
+        
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        // one way to do this.  SceneManager.LoadScene(0);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
